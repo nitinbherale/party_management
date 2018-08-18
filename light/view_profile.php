1ec55d58970837b0b40c744393447028280f199f
nitinbherale <nitinbherale@gmail.com>
2018-08-18 17:25:02 +0530

    two file added

---
+++ light/view_profile.php
@@ -0,0 +1,180 @@
+<?php include("header.php") ?>
+<section class="content profile-page">    
+    <div class="container">
+        <div class="row clearfix">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="body block-header">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-12 col-sm-12">
+                                <h2>Profile</h2>
+                                <ul class="breadcrumb p-l-0 p-b-0 ">
+                                    <li class="breadcrumb-item"><a href="index.php"><i class="icon-home"></i></a></li>
+                                    <li class="breadcrumb-item"><a href="javascript:void(0);">Member List</a></li>
+                                    <li class="breadcrumb-item active">Profile</li>
+                                </ul>
+                            </div>            
+                            <div class="col-lg-6 col-md-12 col-sm-12">
+                                <ul class="nav nav-tabs">
+                                    <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#overview">Overview</a></li>
+                                    <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#schedule">Export PDF</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12">
+                <div class="card active-bg text-white" style="background: #191f28;">
+                    <div class="body profile-header">
+                        <img src="../assets/images/nitin_sb.jpg" class="user_pic rounded img-raised" alt="User">
+                        <div class="detail">
+                            <div class="u_name">
+                                <h4 style="color: #fff;"><strong>Nitin</strong> Bherale</h4>
+                                <span style="color: #fff;">MLA, Murbad Matdarsangh</span> <hr />
+                            </div>
+                            <div id="m_area_chart"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane" id="overview">
+                        <div class="row clearfix">
+                            <div class="col-lg-3 col-md-6 col-6">
+                                <div class="card text-center">
+                                    <div class="body">
+                                        <h5 class="m-b-0">24th July 1993</h5>
+                                         <small>DOB</small>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-6">
+                                <div class="card text-center">
+                                    <div class="body">                            
+                                        <h5 class="m-b-0">Kalyan</h5>
+                                        <small>City</small>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-6">
+                                <div class="card text-center">
+                                    <div class="body">
+                                        <h5 class="m-b-0">421301</h5>
+                                        <small>Pincode</small>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-6">
+                                <div class="card text-center">
+                                    <div class="body">
+                                        <h5 class="m-b-0">Thane</h5>
+                                        <small>Dist</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row clearfix">
+                            <div class="col-lg-4 col-md-12">
+                                <div class="card">
+                                    <div class="header">
+                                        <h2><strong>Info</strong></h2>
+                                    </div>
+                                    <div class="body">
+                                        <small class="text-muted">Address: </small>
+                                        <p>At- Apti, Tal-Kalyan, Post-Vaholi,Dist-Thane, Maharashtra</p>
+                                        <div>
+                                            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d60271.345226256526!2d73.0951300290857!3d19.24061520800938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sapti+kalyan!5e0!3m2!1sen!2sin!4v1534514439942" width="100%" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                        </div>
+                                        <hr>
+                                        <small class="text-muted">Email address: </small>
+                                        <p>nitin.bherale@nmpl.biz</p>
+                                        <hr>
+                                        <small class="text-muted">Mobile No.: </small>
+                                        <p>+91 9922854416</p>
+                                        <hr>
+                                        <small class="text-muted">Whatsapp No.: </small>
+                                        <p>+91 9922854416</p>
+                                        <hr>
+                                        <small class="text-muted">Birth Date: </small>
+                                        <p class="m-b-0">July 24th, 1993</p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            <div class="col-lg-8 col-md-12">
+                                <div class="card">
+                                    <div class="header">
+                                        <h2><strong>Send</strong> Message</h2>
+                                    </div>
+                                    <div class="body m-b-10">
+                                        <div class="form-group">
+                                            <textarea rows="4" class="form-control no-resize" placeholder="Please type what you want..."></textarea>
+                                        </div>
+                                        <div class="post-toolbar-b">
+                                            <button class="btn btn-warning btn-icon btn-icon-mini btn-round"><i class="zmdi zmdi-attachment"></i></button>
+                                            <button class="btn btn-warning btn-icon btn-icon-mini btn-round"><i class="zmdi zmdi-camera"></i></button>
+                                            <button class="btn btn-primary btn-round">Send </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="body">                               
+                                        <h5 class="m-t-20 m-b-0 post_title">Short Info</h5>                                
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>                                                              
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--<div role="tabpanel" class="tab-pane page-calendar active" id="schedule">
+                    </div>-->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Default Size -->
+<div class="modal fade" id="addevent" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="title" id="defaultModalLabel">Add Event</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <div class="form-line">
+                        <input type="number" class="form-control" placeholder="Event Date">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="form-line">
+                        <input type="text" class="form-control" placeholder="Event Title">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="form-line">
+                        <textarea class="form-control no-resize" placeholder="Event Description..."></textarea>
+                    </div>
+                </div>       
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary btn-round waves-effect">Add</button>
+                <button type="button" class="btn btn-simple btn-round waves-effect" data-dismiss="modal">CLOSE</button>
+            </div>
+        </div>
+    </div>
+</div>
