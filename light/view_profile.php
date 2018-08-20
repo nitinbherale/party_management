6d0a9a34ae1c65bbdd43a9e4c59ba4680e15ab67
nitinbherale <nitinbherale@gmail.com>
2018-08-20 17:28:26 +0530

    new updates

--- light/view_profile.php
+++ light/view_profile.php
@@ -1,12 +1,13 @@
 <?php include("header.php");
  if(!isValidUser())  
     {
-    redirect("login.php"); 
+        redirect("login.php"); 
     }
     else
     {
         $mem_id = $_POST['mbr_no'];
-    } ?>
+    }   
+?>
 <section class="content profile-page">    
     <div class="container">
         <div class="row clearfix">
