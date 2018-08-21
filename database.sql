62da6d6b04adc9a54482122c6e0961d77809c0db
nitinbherale <nitinbherale@gmail.com>
2018-08-21 19:40:03 +0530

    changes

--- database.sql
+++ database.sql
@@ -35,7 +35,62 @@ CREATE TABLE `tbl_adm` (
 
 /*Data for the table `tbl_adm` */
 
-insert  into `tbl_adm`(`ad_id`,`ad_f_nm`,`ad_usr_nm`,`ad_pwd`,`ad_mob_no`,`ad_mail`,`ad_type`,`ad_active`,`ad_pic`,`ad_sess`) values (1,'Akshata Pawar','akshata','675737b908ed6fa14bb6c3d7157f49d6','8652598488','akshatapawar129@gmail.com',0,0,NULL,'eilv5h94p9od6f6mrb4r1g9f03');
+insert  into `tbl_adm`(`ad_id`,`ad_f_nm`,`ad_usr_nm`,`ad_pwd`,`ad_mob_no`,`ad_mail`,`ad_type`,`ad_active`,`ad_pic`,`ad_sess`) values (1,'Akshata Pawar','akshata','675737b908ed6fa14bb6c3d7157f49d6','8652598488','akshatapawar129@gmail.com',0,0,NULL,'e5fjfcjna2g6artcc7puumpd11');
+
+/*Table structure for table `tbl_evnt` */
+
+DROP TABLE IF EXISTS `tbl_evnt`;
+
+CREATE TABLE `tbl_evnt` (
+  `evnt_id` int(11) NOT NULL AUTO_INCREMENT,
+  `evnt_tit` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `evnt_des` longtext COLLATE utf8_unicode_ci,
+  `evnt_date` date DEFAULT NULL,
+  `evnt_time` time DEFAULT NULL,
+  `evnt_str` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `evnt_cty` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `evnt_pin_cod` decimal(10,0) DEFAULT NULL,
+  `evnt_coor_per` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `evnt_coor_mail` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `evnt_mob_no` decimal(10,0) DEFAULT NULL,
+  `evnt_pic` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `evnt_appr` int(11) DEFAULT '0',
+  `evnt_appr_time` datetime DEFAULT NULL,
+  `evnt_appr_by` int(11) DEFAULT NULL,
+  `evnt_dis_appr_time` datetime DEFAULT NULL,
+  `evnt_dis_appr_by` int(11) DEFAULT NULL,
+  `evnt_active` int(11) DEFAULT '0',
+  `evnt_edit_time` datetime DEFAULT NULL,
+  `evnt_add_time` datetime DEFAULT NULL,
+  `evnt_assg_to` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `evnt_assg_time` datetime DEFAULT NULL,
+  `evnt_assg` int(11) DEFAULT '0',
+  `evnt_noti` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `evnt_cnl` int(11) DEFAULT '0',
+  PRIMARY KEY (`evnt_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tbl_evnt` */
+
+/*Table structure for table `tbl_master` */
+
+DROP TABLE IF EXISTS `tbl_master`;
+
+CREATE TABLE `tbl_master` (
+  `app_title` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `app_meta_key` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `app_meta_des` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `app_favicon` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `app_logo` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `app_head_name` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `app_head_email` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `app_head_m_no` decimal(10,0) DEFAULT NULL,
+  `app_send_by` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tbl_master` */
+
+insert  into `tbl_master`(`app_title`,`app_meta_key`,`app_meta_des`,`app_favicon`,`app_logo`,`app_head_name`,`app_head_email`,`app_head_m_no`,`app_send_by`) values ('Shivsena Party Management','Shivsena,Yuvasena','Shivsena Program Management Application','favicon.ico','logo.png','Aaditya Thackeray','akshatapawar1292gmail.com','8652598488','ShivSena Digital Media Team');
 
 /*Table structure for table `tbl_member` */
 
@@ -66,7 +121,26 @@ CREATE TABLE `tbl_member` (
 
 /*Data for the table `tbl_member` */
 
-insert  into `tbl_member`(`mem_id`,`mem_f_nm`,`mem_desn`,`mem_email`,`mem_m_no`,`mem_wp_no`,`mem_dis`,`mem_tah`,`mem_str`,`mem_cty`,`mem_ps_cd`,`mem_gen`,`mem_dob`,`mem_fb_lk`,`mem_tw_lk`,`mem_img`,`mem_srt_info`,`mem_grp`,`mem_active`) values (1,'Akshata Pawar','web developer','akshatapawar129@gmail.com','8652598489','8652598487','mumbai suburban','kurla','sangharsh nagar','mumbai','402563',2,'2018-08-15','https://www.facebook.com/ThaneVartaNews/','https://twitter.com/akshatapawar129','1534761393_16 AUGSCORE BOARD.jpg','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.','1,2',0),(2,'nitin bherale','web designer','nitinbherale@gmail.com','8652598488','8652598488','Thane','Thane','Gokhale Road','Thane','402365',1,'2018-01-10','','',NULL,'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.','',0);
+insert  into `tbl_member`(`mem_id`,`mem_f_nm`,`mem_desn`,`mem_email`,`mem_m_no`,`mem_wp_no`,`mem_dis`,`mem_tah`,`mem_str`,`mem_cty`,`mem_ps_cd`,`mem_gen`,`mem_dob`,`mem_fb_lk`,`mem_tw_lk`,`mem_img`,`mem_srt_info`,`mem_grp`,`mem_active`) values (1,'Akshata Pawar','web developer','akshatapawar129@gmail.com','8652598488','8652598487','mumbai suburban','kurla','sangharsh nagar','mumbai','402563',2,'2018-08-15','https://www.facebook.com/ThaneVartaNews/','https://twitter.com/akshatapawar129','1534761393_16 AUGSCORE BOARD.jpg','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.','1,2',0),(2,'nitin bherale','web designer','nitinbherale@gmail.com','8652598488','8652598488','Thane','Thane','Gokhale Road','Thane','402365',1,'2018-01-10','','',NULL,'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.','',1);
+
+/*Table structure for table `tbl_sms` */
+
+DROP TABLE IF EXISTS `tbl_sms`;
+
+CREATE TABLE `tbl_sms` (
+  `sms_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sms_sender_id` int(11) DEFAULT NULL,
+  `sms_sender_usnm` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sms_rece_id` int(11) DEFAULT NULL,
+  `sms_rece_mn` decimal(10,0) DEFAULT NULL,
+  `sms_sent_time` datetime DEFAULT NULL,
+  `sms_content` longtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`sms_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tbl_sms` */
+
+insert  into `tbl_sms`(`sms_id`,`sms_sender_id`,`sms_sender_usnm`,`sms_rece_id`,`sms_rece_mn`,`sms_sent_time`,`sms_content`) values (1,1,'akshata',2,'8652598488','2018-08-21 13:26:55',NULL),(2,1,'akshata',2,'8652598488','2018-08-21 13:30:03','This is my message\r\n\nShivSena Digital Media Team'),(3,1,'akshata',2,'8652598488','2018-08-21 13:32:39','This is my program\nShivSena Digital Media Team');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
